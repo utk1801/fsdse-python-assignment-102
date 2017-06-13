32dd998552e5526d6b8fa4832b19fa870aaba11d
utk1801 <utk1801@users.noreply.github.com>
2017-06-13 20:49:30 +0000

    Done

--- build.py
+++ build.py
@@ -3,8 +3,7 @@ def is_rotation(s1,s2):
     if s1=='' and len(s2)!=0:return False
     if s1=='' and s2=='':return True
     if len(s1)!=len(s2):return False
-    for i in range(len(s1)):
-            if s1[i:] and s2[:i]:
-                return True
+
+    return(len(s1) == len(s2) and s1 in s2*2)
 
 print is_rotation('hello','lohel')
